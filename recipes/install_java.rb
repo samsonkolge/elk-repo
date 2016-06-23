0a5cc50d2a575e5da3b7f3d608d5562f3335d93d
Kolge <samson.sud.kolge@hpe.com>
2016-06-23 18:56:41 +0530

    Stuck on getting the repos installed

---
+++ recipes/install_java.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: elk-repo
+# Recipe:: install_java
+#
+# Add the Oracle Java PPA to apt:
+
+#	sudo add-apt-repository -y ppa:webupd8team/java
+#
+#Update your apt package database:
+
+#	sudo apt-get update
+
+#Install the latest stable version of Oracle Java 8 with this command (and accept the license agreement that pops up):
+
+#	sudo apt-get -y install oracle-java8-installer
+#Now that Java 8 is installed, let's install ElasticSearch.
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
